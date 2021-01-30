603b633fa27bca39ce4ca98567d59155ba7bd785
Richard George <richard@phase.org>
2021-01-30 08:30:15 -0500

    Further links, styling

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ mawild redirector
 ### Dev info
 
  - https://www.serverless.com/blog/serverless-api-gateway-domain
+ - https://stackoverflow.com/questions/43236152/how-to-make-aws-api-gateway-accept-http-instead-of-https
+
+Routing is r53 alias => cloudfront => api gateway => lambda
--- handler.py
+++ handler.py
@@ -1,5 +1,5 @@
 redirects = [
-    ["group", "Facebook Group", "https://www.facebook.com/groups/MAWildlife"],
+    ["fb", "Facebook Group", "https://www.facebook.com/groups/MAWildlife"],
     ["rules", "Facebook Group Rules",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-rules-details/3045269308934439/"],
     ["rulesdetails", None,
@@ -8,9 +8,11 @@ redirects = [
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-faq/306350497440553/"],
     ["owls", "Owl Observation Policy",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
-    ["mawildcares", "Vulnerable / Injured Animal Guidance",
+    ["care", "Vulnerable / Injured Animal Guidance",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
     ["cares", None,
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
+    ["mawildcares", None,
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
     ["carepdf", "Vulnerable / Injured Animal Guidance, non-private version (PDF)",
      "https://www.dropbox.com/s/0atgonhehc960mz/MAwildGuide.pdf"],
@@ -21,14 +23,45 @@ redirects = [
     ["moderateyournope", "Phobia Handling Request",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
     ["pinned", "Group Pinned Post", "https://www.facebook.com/groups/MAWildlife/permalink/649606069019913/"],
+    ["members", "Group Members and Administration Team", "https://www.facebook.com/groups/MAWildlife/members"]
 ]
+
+header = """<!doctype html>
+<html lang="en"><head><title>mawild.link</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<style type="text/css">
+ a { color: #777; text-decoration: underline; text-decoration-style: dotted; }
+ a:hover { color: #444; text-decoration-style: solid }
+</style>
+</head>
+<body><div class="container">
+<h1>mawild.link redirection service</h1>
+<p>This is a link-shortening service for the Massachusetts Wildlife Facebook Group,
+ which is run by the <a href="http://mawildlifecollective.com">Massachusetts Wildlife Collective</a></p>
+"""
+
+footer = """</div>
+    <footer class="footer" 
+    style="position: absolute; bottom: 0; width: 100%; margin-top: 20px; height: 56px;line-height: 56px;">
+        <div class="container-fluid">
+            <div class="gh-link" style=" padding-right: 4px; float: right"><a
+                    class="text-dark" href="https://github.com/parsingphase"><i
+                    class="fab fa-github"></i></a></div>
+            <div style="float: right; padding-right: 16px;">
+                mawild.link created by <a class="text-dark" href="https://parsingphase.dev">parsingphase</a>
+            </div>
+        </div>
+    </footer>
+</body></html>"""
 
 
 def redirect(event, context):
     response = None
+    stub = None
 
     if "rawPath" in event:
-        stub = event["rawPath"].lstrip('/')
+        stub = event["rawPath"].lstrip('/').lower()
         for link in redirects:
             if link[0] == stub:
                 response = {
@@ -48,13 +81,21 @@ def redirect(event, context):
             ]
         )
 
+        message = '' if stub is None or stub == '' \
+            else f'<div class="alert alert-warning" role="alert">The short link <code>mawild.link/{stub}</code> was not found</div>'
+
         response = {
             "statusCode": 200,
             "headers": {
                 "Content-Type": "text/html; charset=utf-8"
             },
-            "body": f'<p>Link not found. Available options:</p><ul>{link_list}</ul>'
+            "body": f'{header}{message}<ul>{link_list}</ul>'
                     '<p>Note that most links will require you to be a group member</p>'
+                    f'{footer}'
         }
 
     return response
+
+
+if __name__ == '__main__':
+    print(redirect({}, {})['body'])
