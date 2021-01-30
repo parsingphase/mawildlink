7c060c75488745a19fe713fdeab7179cf5c4d72e
Richard George <richard@phase.org>
2021-01-29 21:07:21 -0500

    Add remaining links

--- handler.py
+++ handler.py
@@ -2,13 +2,25 @@ redirects = [
     ["group", "Facebook Group", "https://www.facebook.com/groups/MAWildlife"],
     ["rules", "Facebook Group Rules",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-rules-details/3045269308934439/"],
+    ["rulesdetails", None,
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-rules-details/3045269308934439/"],
     ["faq", "Group FAQ",
      "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-faq/306350497440553/"],
-    ["pinned", "Group Pinned Post", "https://www.facebook.com/groups/MAWildlife/permalink/649606069019913/"],
+    ["owls", "Owl Observation Policy",
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
+    ["mawildcares", "Vulnerable / Injured Animal Guidance",
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
+    ["cares", None,
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
+    ["carepdf", "Vulnerable / Injured Animal Guidance, non-private version (PDF)",
+     "https://www.dropbox.com/s/0atgonhehc960mz/MAwildGuide.pdf"],
     ["philosophy", "Moderation Philosophy",
      "https://www.facebook.com/notes/massachusetts-wildlife/moderation-philosophy-for-massachusetts-wildlife/753679958712237/"],
-    ["dslrwild", "Guide to DSLR photography",
-     "https://parsingphase.medium.com/some-notes-on-wildlife-photography-6370ea4f8965"]
+    ["dslrwild", "Guide To DSLR Photography (by an admin, not a group document)",
+     "https://parsingphase.medium.com/some-notes-on-wildlife-photography-6370ea4f8965"],
+    ["moderateyournope", "Phobia Handling Request",
+     "https://www.facebook.com/notes/massachusetts-wildlife/massachusetts-wildlife-owl-observation-policy/845927992925738/"],
+    ["pinned", "Group Pinned Post", "https://www.facebook.com/groups/MAWildlife/permalink/649606069019913/"],
 ]
 
 
@@ -32,7 +44,7 @@ def redirect(event, context):
         link_list = "\n".join(
             [
                 f'<li>{l[1]}: <a href="/{l[0]}">mawild.link/{l[0]}</a></li>'
-                for l in redirects
+                for l in redirects if l[1] is not None
             ]
         )
 
@@ -42,6 +54,7 @@ def redirect(event, context):
                 "Content-Type": "text/html; charset=utf-8"
             },
             "body": f'<p>Link not found. Available options:</p><ul>{link_list}</ul>'
+                    '<p>Note that most links will require you to be a group member</p>'
         }
 
     return response
